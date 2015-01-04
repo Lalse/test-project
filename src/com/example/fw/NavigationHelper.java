3ce7b2cdf1a3bc5f553b3e60a9a27af2e4f9318b
Irina <lasval86@gmail.com>
2015-01-04 17:11:32 +0200

    Revert "Revert "Homework 3""
    This reverts commit d9e6eeb4c7acbe7e90f33ca4acbb2aa80863bd3e.

---
+++ src/com/example/fw/NavigationHelper.java
@@ -0,0 +1,31 @@
+package com.example.fw;
+
+import org.openqa.selenium.By;
+
+public class NavigationHelper extends HelperBase {
+
+	public NavigationHelper(ApplicationManager manager) {
+		super(manager);
+		
+	}
+
+	public void openMainPage() {
+		driver.get(manager.baseUrl + "/addressbookv4.1.4/");
+	}
+
+	public void gotoHome() {
+		click(By.linkText("home"));
+	}
+
+	public void gotoGroupPage() {
+		click(By.linkText("groups"));
+	}
+
+	public void returnToGroupPage() {
+		click(By.linkText("group page"));
+		
+	}
+
+	
+
+}
